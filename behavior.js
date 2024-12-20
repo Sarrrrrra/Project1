b6ba15db6258972d158c74c0c41d2ed00b7d81d8
Sarrrrrra <elimrani.sara1@gmail.com>
2024-12-20 21:50:49 +0100

    Add files via upload

---
+++ behavior.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    alert('Hello! Welcome to Medina & Tapas');
+});
+
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('header');
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+function toggleMenu() {
+    const menuToggle = document.querySelector('.menuToggle');
+    const navbar = document.querySelector('.navbar');
+    navbar.classList.toggle('active');
+    menuToggle.classList.toggle('active');
+}
